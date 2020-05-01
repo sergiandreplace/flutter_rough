b5068104549ac09056d5f1101a83fb799c62acfe
Sergi Martínez <sergi.martinez@gmail.com>
2020-05-01 22:01:24 +0200

    filler reorganized

--- lib/src/filler/filler.dart
+++ lib/src/filler/filler.dart
@@ -1,8 +1,7 @@
 import 'dart:math';
 
-import 'package:rough/src/filler/scan_line_hachure.dart';
-
 import '../core.dart';
+import '../geometry.dart';
 import '../renderer.dart';
 
 class IntersectionInfo {
@@ -12,9 +11,140 @@ class IntersectionInfo {
   IntersectionInfo({this.point, this.distance});
 }
 
-abstract class _LineFiller {
+abstract class Filler {
+  OpSet fill(List<PointD> points, Options o);
+
+  List<Line> buildFillLines(List<PointD> points, Options o) {
+    PointD rotationCenter = PointD(0, 0);
+    double angle = (o.hachureAngle + 90).roundToDouble();
+    if (angle != 0) {
+      points = rotatePoints(points, rotationCenter, angle);
+    }
+    List<Line> lines = _straightenLines(points, o);
+    if (angle != 0) {
+      lines = rotateLines(lines, rotationCenter, -angle);
+    }
+    return lines;
+  }
+
+  List<Line> _straightenLines(List<PointD> points, Options o) {
+    List<PointD> vertices = points ?? [];
+    List<Line> lines = [];
+    if (vertices[0] != vertices[vertices.length - 1]) {
+      vertices.add(vertices[0]);
+    }
+    if (vertices.length > 2) {
+      double gap = o.hachureGap;
+      gap = max(gap, 0.1);
+
+      // Create sorted edges table
+      List<Edge> edges = [];
+      for (int i = 0; i < vertices.length - 1; i++) {
+        PointD p1 = vertices[i];
+        PointD p2 = vertices[i + 1];
+        if (p1.x != p2.x) {
+          double yMin = min(p1.y, p2.y);
+          edges.add(Edge(yMin: yMin, yMax: max(p1.y, p2.y), x: yMin == p1.y ? p1.x : p2.x, isLope: (p2.x - p1.x) / (p2.y - p1.y)));
+        }
+      }
+      edges.sort((e1, e2) {
+        if (e1.yMin < e2.yMin) {
+          return -1;
+        } else if (e1.yMin > e2.yMin) {
+          return 1;
+        } else if (e1.x < e2.x) {
+          return -1;
+        } else if (e1.x > e2.x) {
+          return 1;
+        } else if (e1.yMax == e2.yMax) {
+          return 0;
+        } else {
+          return ((e1.yMax - e2.yMax) / ((e1.yMax - e2.yMax).abs())).ceil();
+        }
+      });
+      if (edges.isEmpty) {
+        return lines;
+      }
+      // Start scanning
+      List<ActiveEdge> activeEdges = [];
+      double y = edges[0].yMin;
+      while (activeEdges.isNotEmpty || edges.isNotEmpty) {
+        if (edges.isNotEmpty) {
+          int ix = -1;
+          for (int i = 0; i < edges.length; i++) {
+            if (edges[i].yMin > y) {
+              break;
+            }
+            ix = i;
+          }
+          List<Edge> removed = edges.sublist(0, ix + 1);
+          edges.removeRange(0, ix + 1);
+          removed.forEach((edge) {
+            activeEdges.add(ActiveEdge(y, edge));
+          });
+        }
+        activeEdges = activeEdges.where((ae) => ae.edge.yMax > y).toList();
+        activeEdges.sort((ae1, ae2) {
+          if (ae1.edge.x == ae2.edge.x) {
+            return 0;
+          }
+          return ((ae1.edge.x - ae2.edge.x) / ((ae1.edge.x - ae2.edge.x)).abs()).ceil();
+        });
+
+        // fill between the edges
+        if (activeEdges.length > 1) {
+          for (int i = 0; i < activeEdges.length; i = i + 2) {
+            int nexti = i + 1;
+            if (nexti >= activeEdges.length) {
+              break;
+            }
+            Edge ce = activeEdges[i].edge;
+            Edge ne = activeEdges[nexti].edge;
+            lines.add(Line(PointD(ce.x.roundToDouble(), y), PointD(ne.x.roundToDouble(), y)));
+          }
+        }
+
+        y += gap;
+        activeEdges = activeEdges.map((ae) {
+          return ActiveEdge(ae.s, ae.edge.copyWith(x: ae.edge.x + (gap * ae.edge.isLope)));
+        }).toList();
+      }
+    }
+    return lines;
+  }
+}
+
+class Edge {
+  double yMin;
+  double yMax;
+  double x;
+  double isLope;
+
+  Edge({this.yMin, this.yMax, this.x, this.isLope});
+
+  Edge copyWith({double yMin, double yMax, double x, double isLope}) => Edge(
+        yMin: yMin ?? this.yMin,
+        yMax: yMax ?? this.yMax,
+        x: x ?? this.x,
+        isLope: isLope ?? this.isLope,
+      );
+
+  @override
+  String toString() {
+    return 'Edge{yMin: $yMin, yMax: $yMax, x: $x, isLope: $isLope}';
+  }
+}
+
+class ActiveEdge {
+  double s;
+  Edge edge;
+
+  ActiveEdge(this.s, this.edge);
+}
+
+abstract class BaseLineFiller extends Filler {
   OpSet fillPolygon(List<PointD> points, Options o, bool connectEnds) {
-    List<Line> lines = polygonHachureLines(points, o);
+    List<Line> lines = buildFillLines(points, o);
     if (connectEnds) {
       List<Line> connectingLines = connectLines(points, lines);
       lines += connectingLines;
@@ -101,19 +231,19 @@ abstract class _LineFiller {
   }
 }
 
-class HachureFiller extends _LineFiller {
+class HachureFiller extends BaseLineFiller {
   OpSet fill(List<PointD> points, Options o) {
     return fillPolygon(points, o, false);
   }
 }
 
-class ZigZagFiller extends _LineFiller {
+class ZigZagFiller extends BaseLineFiller {
   OpSet fill(List<PointD> points, Options o) {
     return fillPolygon(points, o, true);
   }
 }
 
-class HatchFiller extends _LineFiller {
+class HatchFiller extends BaseLineFiller {
   OpSet fill(List<PointD> points, Options o) {
     OpSet set1 = fillPolygon(points, o, false);
     Options rotated = o.copyWith(hachureAngle: o.hachureAngle + 90);
@@ -122,15 +252,15 @@ class HatchFiller extends _LineFiller {
   }
 }
 
-class DashedFiller {
+class DashedFiller extends Filler {
   OpSet fill(List<PointD> points, Options o) {
-    List<Line> lines = polygonHachureLines(points, o);
+    List<Line> lines = buildFillLines(points, o);
     return OpSet(type: OpSetType.fillSketch, ops: dashedLines(lines, o));
   }
 
   List<Op> dashedLines(List<Line> lines, Options o) {
-    double offset = o.dashOffset < 0 ? (o.hachureGap < 0 ? (o.strokeWidth * 4) : o.hachureGap) : o.dashOffset;
-    double gap = o.dashGap < 0 ? (o.hachureGap < 0 ? (o.strokeWidth * 4) : o.hachureGap) : o.dashGap;
+    double offset = o.dashOffset;
+    double gap = o.dashGap;
     List<Op> ops = [];
     lines.forEach((line) {
       double length = line.length;
@@ -160,24 +290,18 @@ class DashedFiller {
   }
 }
 
-class DotFiller {
+class DotFiller extends Filler {
   OpSet fill(List<PointD> points, Options o) {
     o = o.copyWith(curveStepCount: 4, hachureAngle: 0, roughness: 1);
-    List<Line> lines = polygonHachureLines(points, o);
+    List<Line> lines = buildFillLines(points, o);
     return dotsOnLines(lines, o);
   }
 
   OpSet dotsOnLines(List<Line> lines, Options o) {
     List<Op> ops = [];
     double gap = o.hachureGap;
-    if (gap < 0) {
-      gap = o.strokeWidth * 4;
-    }
     gap = max(gap, 0.1);
     double fweight = o.fillWeight;
-    if (fweight < 0) {
-      fweight = o.strokeWidth / 2;
-    }
     double ro = gap / 4;
     lines.forEach((line) {
       double length = line.length;
