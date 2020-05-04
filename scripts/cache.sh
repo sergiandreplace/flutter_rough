f3aa87c8774d097f1ff10ea32c3a7753ac1c3a76
Sergi Martínez <sergi.martinez@gmail.com>
2020-05-05 00:09:45 +0200

    trying with secrets

---
+++ scripts/cache.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+file_location=~/.pub-cache/credentials.json
+if [ -e $policy ]; then
+  echo "File $1.json already exists!"
+else
+  cat > $file_location <<EOF
+$CACHED_JSON
+EOF
+echo $CACHED_JSON
+fi
